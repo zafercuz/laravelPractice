5de8c9a3caf0520fc93e8e960c62f8aca767128b
Your Name <beltondavid1@gmail.com>
2019-09-20 17:52:34 +0800

    Up to 3:48:55 in the video
    new file:   app/Http/Controllers/FollowsController.php
    new file:   app/Http/Controllers/PostsController.php
    new file:   app/Http/Controllers/ProfilesController.php
    new file:   app/Policies/ProfilePolicy.php
    new file:   app/Post.php
    new file:   app/Profile.php
    modified:   app/User.php
    modified:   composer.json
    modified:   composer.lock
    modified:   config/database.php
    new file:   database/migrations/2019_09_20_005619_create_profiles_table.php
    new file:   database/migrations/2019_09_20_013630_create_posts_table.php
    new file:   database/migrations/2019_09_20_085857_creates_profile_user_pivot_table.php
    modified:   public/js/app.js
    modified:   resources/js/app.js
    deleted:    resources/js/components/ExampleComponent.vue
    new file:   resources/js/components/FollowButton.vue
    deleted:    resources/views/home.blade.php
    modified:   resources/views/layouts/app.blade.php
    new file:   resources/views/posts/create.blade.php
    new file:   resources/views/posts/show.blade.php
    new file:   resources/views/profiles/edit.blade.php
    new file:   resources/views/profiles/index.blade.php
    modified:   routes/web.php

---
+++ app/Profile.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Profile extends Model
+{
+    protected $guarded = [];
+
+    public function profileImage(){
+        $imagePath = ($this->image) ? $this->image : 'profile/vfXyBcQeW8kx7EOu2PDAJJchmnKbDC11Z5K0W8TS.jpeg';
+        return '/storage/' . $imagePath;
+    }
+
+    public function followers(){
+        return $this->belongsToMany(User::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
--- app/User.php
+++ app/User.php
@@ -36,4 +36,27 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    protected static function boot(){
+        parent::boot();
+
+        static::created(function ($user) {
+            $user->profile()->create([
+                'title' => $user->username,
+            ]);
+        });
+    }
+
+    public function posts(){
+        return $this->hasMany(Post::class)->orderBy('created_at', 'DESC');
+    }
+
+    public function following(){
+        return $this->belongsToMany(Profile::class);
+    }
+
+    public function profile(){
+        return $this->hasOne(Profile::class);
+    }
+
 }
