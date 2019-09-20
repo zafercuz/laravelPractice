e297b23adb6efed8660fa8f4fa2b119820b025a2
Your Name <beltondavid1@gmail.com>
2019-09-20 17:55:11 +0800

    Add count() in index blade
    modified:   resources/views/profiles/index.blade.php

--- resources/views/profiles/index.blade.php
+++ resources/views/profiles/index.blade.php
@@ -28,7 +28,7 @@
       <div class="d-flex">
         <div class="pr-5"><strong>{{ $user->posts->count() }}</strong> posts</div>
         <div class="pr-5"><strong>{{ $user->profile->followers->count() }}</strong> followers</div>
-        <div class="pr-5"><strong>{{ $user->following }}</strong> following</div>
+        <div class="pr-5"><strong>{{ $user->following->count() }}</strong> following</div>
       </div>
       <div class="pt-4 font-weight-bold">{{ $user->profile->title }}</div>
       <div>{{ $user->profile->description }}</div>
