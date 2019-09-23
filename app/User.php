fbd01d21e68c46922824a46e41e016b9f499456d
Your Name <beltondavid1@gmail.com>
2019-09-23 12:00:12 +0800

    Final
    modified:   app/Http/Controllers/Auth/LoginController.php
    modified:   app/Http/Controllers/Auth/RegisterController.php
    modified:   app/Http/Controllers/PostsController.php
    modified:   app/Http/Controllers/ProfilesController.php
    new file:   app/Mail/NewUserWelcomeMail.php
    new file:   app/Providers/TelescopeServiceProvider.php
    modified:   app/User.php
    modified:   composer.json
    modified:   composer.lock
    modified:   config/app.php
    new file:   config/telescope.php
    new file:   public/vendor/telescope/app-dark.css
    new file:   public/vendor/telescope/app.css
    new file:   public/vendor/telescope/app.js
    new file:   public/vendor/telescope/favicon.ico
    new file:   public/vendor/telescope/mix-manifest.json
    new file:   resources/views/emails/welcome-email.blade.php
    modified:   resources/views/layouts/app.blade.php
    new file:   resources/views/posts/index.blade.php
    modified:   routes/web.php

--- app/User.php
+++ app/User.php
@@ -5,6 +5,8 @@ namespace App;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Support\Facades\Mail;
+use App\Mail\NewUserWelcomeMail;
 
 class User extends Authenticatable
 {
@@ -44,6 +46,9 @@ class User extends Authenticatable
             $user->profile()->create([
                 'title' => $user->username,
             ]);
+
+            Mail::to($user->email)->send(new NewUserWelcomeMail());
+
         });
     }
 
